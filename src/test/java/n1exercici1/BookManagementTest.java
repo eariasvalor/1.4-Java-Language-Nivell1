2bb137444b37e8ce575537e496f6798dccc35fd5
Esther Arias <eariasvalor@gmail.com>
2025-10-03 18:42:57 +0200

    feat: added tests for list size and checking title positions

--- src/test/java/n1exercici1/BookManagementTest.java
+++ src/test/java/n1exercici1/BookManagementTest.java
@@ -6,8 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookManagementTest {
     @Test
-    void BookListIsNotNull(){
+    void bookListIsNotNull(){
         BookManagement bookManagement = new BookManagement();
         assertNotNull(bookManagement.getBookList());
     }
+
+    @Test
+    void listHasSizeThreeWhenAddingThreeBooks(){
+        BookManagement bookManagement = new BookManagement();
+        bookManagement.addBook(new Book("First Book"));
+        bookManagement.addBook(new Book("Second Book"));
+        bookManagement.addBook(new Book("Third Book"));
+        assertEquals(3, bookManagement.getBookList().size());
+    }
+
+    @Test
+    void listHasSecondBookInPositionOne(){
+        BookManagement bookManagement = new BookManagement();
+        bookManagement.addBook(new Book("First Book"));
+        bookManagement.addBook(new Book("Second Book"));
+        bookManagement.addBook(new Book("Third Book"));
+        assertTrue(bookManagement.getBookByTitle(1).equalsIgnoreCase("Second Book"));
+    }
+
+    @Test
+    void listHasThirdBookInPositionTwo(){
+        BookManagement bookManagement = new BookManagement();
+        bookManagement.addBook(new Book("First Book"));
+        bookManagement.addBook(new Book("Second Book"));
+        bookManagement.addBook(new Book("Third Book"));
+        assertTrue(bookManagement.getBookByTitle(2).equalsIgnoreCase("Third Book"));
+    }
+
 }
